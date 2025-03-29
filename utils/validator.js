d60aa9de373b31713f76a04a100a3e6fb1857dba
Tran Thanh Hien <hienxadoi2020@gmail.com>
2025-03-29 10:06:05 +0700

    Add validation for user authentication and management routes

--- utils/validator.js
+++ utils/validator.js
@@ -1,6 +1,7 @@
 let { body, validationResult } = require('express-validator')
 let constants = require('./constants')
 let util = require('util')
+let { CreateErrorResponse } = require('./responseHandler') 
 
 let options = {
     password:{
@@ -36,7 +37,53 @@ module.exports = {
     ],
     LoginValidator: [
         body("username").isLength(options.username).withMessage("username hoac password sai"),
-        body("password").isStrongPassword(option.password).withMessage("username hoac password sai")
+        body("password").isStrongPassword(options.password).withMessage("username hoac password sai")
+    ],
+
+    
+    CreateUserValidator: [
+        body("username").isLength({min: options.username.minLength}).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME, options.username.minLength)),
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL),
+        body("role").isIn(["user", "mod", "admin"]).withMessage("Role must be user, mod or admin")
+    ],
+
+    UpdateUserValidator: [
+        body("password").optional().isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("email").optional().isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL)
+    ],
+
+    ChangePasswordValidator: [
+        body("oldpassword").notEmpty().withMessage("Old password is required"),
+        body("newpassword").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols))
+    ],
+
+    ForgotPasswordValidator: [
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL)
+    ],
+
+    ResetPasswordValidator: [
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols))
     ]
 }
 // multer
